5cce13e662db001b0bde068180b06c356557d37d
quentin <quent59bd@hotmail.fr>
2019-01-25 14:31:08 +0100

    <version final mathematiques.py>.

--- mathematiques.py
+++ mathematiques.py
@@ -1,42 +1,45 @@
-Methode = input ("Que veux-tu faire ? n-Addition n-Soustraction n-Multiplication n-Division n-Arreter n Marque sans faute et 
-avec une majuscule ce que tu as choisie ... n= ")
-Methode = str (Methode) 
+Methode = input("Que veux-tu faire ? n-Addition n-Soustraction n-Multiplication n-Division n-Arreter n Marque sans faute et avec une majuscule ce que tu as choisie ... n= ")
+Methode = str(Methode)
+while str(Methode) != 'Arreter' :
 
-if str(Methode) == 'Addition': 
-print ("Tu as choisie {}".format (Methode)) 
-x = input ("Nombre 1 = ") 
-y = input("Nombre 2 = ") 
-x = float(x) 
-y = float(y) 
-print("Le resultat de ton addition est {}".format(float(x) + float(y))) 
-print("n n") 
+    Methode = input("Que veux-tu faire ? n-Addition n-Soustraction n-Multiplication n-Division n-Arreter n Marque sans faute et avec une majuscule ce que tu as choisie ... n= ")
+    
+    if str(Methode) == 'Addition': 
+        print("Tu as choisie {}".format (Methode)) 
+        x = input ("Nombre 1 = ") 
+        y = input("Nombre 2 = ") 
+        x = float(x) 
+        y = float(y) 
+        print("Le resultat de ton addition est {}".format(float(x) + float(y))) 
+        print("n n") 
 
-if str(Methode) == 'Sosutraction': 
-print ("Tu as choisie {}".format (Methode)) 
-x = input ("Nombre 1 = ") 
-y = input("Nombre 2 = ") 
-x = float(x) 
-y = float(y) 
-print("Le resultat de ta soustraction est {}".format(float(x) - float(y))) 
-print("n n") 
+    if str(Methode) == 'Soustraction': 
+        print ("Tu as choisie {}".format (Methode)) 
+        x = input ("Nombre 1 = ") 
+        y = input("Nombre 2 = ") 
+        x = float(x) 
+        y = float(y) 
+        print("Le resultat de ta soustraction est {}".format(float(x) - float(y))) 
+        print("n n") 
 
-if str(Methode) == 'Multiplication': 
-print ("Tu as choisie {}".format (Methode)) 
-x = input ("Nombre 1 = ") 
-y = input("Nombre 2 = ") 
-x = float(x) 
-y = float(y) 
-print("Le resultat de ta multiplication est {}".format(float(x) * float(y))) 
-print("n n") 
+    if str(Methode) == 'Multiplication': 
+        print ("Tu as choisie {}".format (Methode)) 
+        x = input ("Nombre 1 = ") 
+        y = input("Nombre 2 = ") 
+        x = float(x) 
+        y = float(y) 
+        print("Le resultat de ta multiplication est {}".format(float(x) * float(y))) 
+        print("n n") 
 
-if str(Methode) == 'Division': 
-print ("Tu as choisie {}".format (Methode)) 
-x = input ("Nombre 1 = ") 
-y = input("Nombre 2 = ") 
-x = float(x) 
-y = float(y) 
-print("Le resultat de ta division est {}".format(float(x) / float(y))) 
-print("n n") 
+    if str(Methode) == 'Division': 
+        print ("Tu as choisie {}".format (Methode)) 
+        x = input ("Nombre 1 = ") 
+        y = input("Nombre 2 = ") 
+        x = float(x) 
+        y = float(y) 
+        print("Le resultat de ta division est {}".format(float(x) / float(y))) 
+        print("n n") 
 
-if str(Methode) == 'Arreter': 
-break
+    if str(Methode) == 'Arreter': 
+        sys.exit()
+    
